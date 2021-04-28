cdec4fb42873a09ca44e5efee5b369bec649d91e
O.T <oleg.tyran@gmail.com>
2021-04-29 01:57:35 +0300

    PS-2021.1.1 <olegt@OT-NB Update baseRefactoring.xml, IntelliLang.xml, ide.general.xml, git.xml, project.default.xml, diff.xml, ea_extended.xml, postfixTemplates.xml, applicationLibraries.xml, NB-inspection-common.xml, ignore.xml, NB-inspection-tamaranga.xml, find.xml, ui.lnf.xml, filetypes.xml, php.xml, projectView.xml, NB-Keymap.xml, colors.scheme.xml, notifications.xml, _@user_Darcula.icls, databaseDrivers.xml, templates.xml, Vue.xml, duplicatesDetector.xml, emmet.xml, NB_CodeStyle_Main.xml, Default.xml, PHP.xml, laf.xml, path.macros.xml, intentionSettings.xml, web-browsers.xml, wsl.distributions.xml, HTTP Request.xml, vcs.xml, editor.xml, usageView.xml, webServers.xml, debugger.xml	Create PHP Class.php, NB-inspect-light.xml, PHP Constructor.php, ui-datetime.xml, OpenAPI Specifications __json_.xml, PHP Field Doc Comment.php, HTML File.html, PHP Trait.php, JavaScript File.js, OT_NB-color-scheme-dark-2.icls, PHP Getter Method.php, PHP Function Doc Comment.php, OpenAPI Specifications __yaml_.xml, PHP Fluent Setter Method.php, PHP Overridden Method Body.php, lightEdit.xml, PHP File Header.php, PHP File Header.php, NB-Dark-Colors.icls, CSS File.css, PHP Setter Method.php, PHP Interface.php, HTML4 File.html, PHP File.php, updates.xml

---
+++ fileTemplates/internal/PHP Class.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Project: ${PROJECT_NAME};
+ * Creator: ${USER};
+ */
+
+#if (${NAMESPACE})
+
+namespace ${NAMESPACE};
+
+class ${NAME} {
+
+}
+
+#else
+
+class ${NAME} {
+
+}
+#end
+
---
+++ fileTemplates/internal/PHP Interface.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Project: ${PROJECT_NAME};
+ * Creator: ${USER}; 
+ *    Date: ${DATE};
+ */
+ 
+#if (${NAMESPACE})
+
+namespace ${NAMESPACE};
+
+interface ${NAME} {
+
+}
+#else
+
+interface ${NAME} {
+
+}
+
+#end
+
